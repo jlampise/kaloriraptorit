7013661be6ff91d8460f370013d9351c8dbb8681
Jussi Lampiselka <jlampise@iki.fi>
2018-09-13 16:50:53 +0300

    Update react-scripts, remove redux-devtools-extension, which was breaking production build

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,18 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import './css/custom.css';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(
-  reducers,
-  {},
-  composeWithDevTools(applyMiddleware(reduxThunk))
-);
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>

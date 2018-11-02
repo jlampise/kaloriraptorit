50a192a47a24dd9e3cd7a96b74e83ace2998d86f
Jussi Lampiselka <jlampise@iki.fi>
2018-11-02 09:10:31 +0200

    Add input for password

---
+++ server/client/src/components/parts/form/PasswordInput.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const TextInput = field => {
+  const {
+    meta: { touched, error }
+  } = field;
+  const className = `form-group ${touched && error ? ' has-error' : ''} `;
+
+  return (
+    <div className={className}>
+      <label>{field.label}</label>
+      <input
+        placeholder={field.placeholder ? field.placeholder : ''}
+        className="form-control"
+        type="password"
+        {...field.input} //generate all input events, like equliant to examples of: onChange={field.input.onChange} or onFocus={field.input.onFocus} or onBlur....
+      />
+      <div className="help-block">{touched ? error : ''}</div>
+    </div>
+  );
+};
+
+export default TextInput;

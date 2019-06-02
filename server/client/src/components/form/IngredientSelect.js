5b5a3579b59690c01a18de331bbe7ff8f133d638
Jussi Lampiselka <jlampise@iki.fi>
2019-06-02 17:52:13 +0300

    Update react-select: 2.4.4 -> 3.0.3

--- server/client/src/components/form/IngredientSelect.js
+++ server/client/src/components/form/IngredientSelect.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AsyncSelect from 'react-select/lib/Async';
+import AsyncSelect from 'react-select/async';
 import axios from 'axios';
 
 export default ({ selectIngredient }) => {
